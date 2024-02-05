82049b8b77c1fb1ea036df4f33d9c5b924239a5e
Roman Kucherenko <roman.kucherenko@vaimo.com>
2024-02-05 16:41:52 +0100

    Add map component with base map; edit README file

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # carto_test-map
 
-An application with a map from CARTO
+A React + TS application with a map from CARTO
+
+## Documentation
+
+The app was created using React Vite as I used it many times and it proved it's simplicity and stability in use.
+The app is hosted on Netlify as it is a free source and have nice integration with GitHub for quick apps deploy.
+
+App link: https://carto-map-test-app.netlify.app/
---
+++ src/Map.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import DeckGL from "@deck.gl/react/typed";
+import StaticMap from "react-map-gl";
+import {
+  BASEMAP,
+  CartoLayer,
+  setDefaultCredentials,
+  MAP_TYPES,
+  API_VERSIONS,
+} from "@deck.gl/carto/typed";
+
+import "./Map.css";
+
+setDefaultCredentials({
+  accessToken:
+    "eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfbnExaGw4czUiLCJqdGkiOiJiNjIxMjNlZiJ9.1whcmZMjThY5gV5aQUHJrievAGAp2RLGOLey1u9dy9g",
+  apiBaseUrl: "https://gcp-us-east1.api.carto.com",
+});
+
+const INITIAL_VIEW_STATE = {
+  longitude: -97.5,
+  latitude: 35.5,
+  zoom: 4,
+  pitch: 0,
+  bearing: 0,
+};
+
+function Map() {
+  const layer = new CartoLayer({
+    type: MAP_TYPES.QUERY,
+    connection: "bigquery",
+    data: "SELECT * FROM carto-demo-data.demo_tables.world_airports",
+    pointRadiusMinPixels: 2,
+    getLineColor: [0, 0, 0, 200],
+    getFillColor: [238, 77, 90],
+    lineWidthMinPixels: 1,
+  });
+
+  return (
+    <>
+      <div className="toolbar">
+        <span>Toolbar</span>
+      </div>
+      <div className="map-wrapper">
+        <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          controller={true}
+          layers={[layer]}
+        >
+          <StaticMap mapStyle={BASEMAP.POSITRON} />
+        </DeckGL>
+      </div>
+    </>
+  );
+}
+
+export default Map;
